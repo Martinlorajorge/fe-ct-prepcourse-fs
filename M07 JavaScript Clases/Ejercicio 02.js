16ae5d7fa4cafa6d05fec762e3e6dca9b4858058
Martin Lora <martinlorajorge@gmail.com>
2023-08-07 19:12:42 -0300

    Ejercicios HomeWork 07  Terminados

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -2,29 +2,60 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 class Persona {
-   // Crea el constructor de la clase "Persona".
-   // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
-   // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
-   // sus valores.
+	// Crea el constructor de la clase "Persona".
+	// Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+	// Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+	// sus valores.
+	constructor(nombre, apellido, edad, domicilio) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.domicilio = domicilio;
+	}
+	detalle() {
+		return {
+			nombre: this.nombre,
+			apellido: this.apellido,
+			edad: this.edad,
+			domicilio: this.domicilio,
+		};
+	}
+	datos() {
+		return this.nombre + ", " + this.edad + " años";
+	}
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
-   // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
-   // Recibirás las propiedades por parámetro.
-   // Retornar la instancia creada.
-   // Tu código:
+	// En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
+	// Recibirás las propiedades por parámetro.
+	// Retornar la instancia creada.
+	// Tu código:
+	var instanciaPersona = new Persona(nombre, apellido, edad, domicilio);
+	return instanciaPersona;
 }
 
 function agregarMetodo() {
-   // La función agrega un método "datos" a la clase "Persona".
-   // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
-   // Ejemplo: "Juan, 22 años".
-   // Tu código:
+	// La función agrega un método "datos" a la clase "Persona".
+	// Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+	// Ejemplo: "Juan, 22 años".
+	// Tu código:
+	class PersonaConDatos extends Persona {
+		constructor(nombre, apellido, edad, domicilio) {
+			super(nombre, apellido, edad, domicilio);
+		}
+
+		datos() {
+			return this.nombre + ", " + this.edad + " años";
+		}
+	}
+
+	// Retornar la clase PersonaConDatos con el método "datos" agregado
+	return PersonaConDatos;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   crearInstanciaPersona,
-   agregarMetodo,
-   Persona,
+	crearInstanciaPersona,
+	agregarMetodo,
+	Persona,
 };
